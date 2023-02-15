54b49dde0a3b160fb470c2fc11d0d3ce5bdd9891
Soslan Aldatov <me@sptm.dev>
2023-02-15 18:48:57 +0400

    move await inside function

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from aiogram import Bot, Dispatcher, types
 token = str(os.environ.get('TELEGRAM_BOT_API_TOKEN'))
 logging.basicConfig(level=logging.DEBUG)
 
-
 bot = Bot(token)
 dispatcher = Dispatcher(bot)
 
@@ -22,5 +21,9 @@ async def unknown(message: types.Message):
     await message.answer("Unknown command or data!")
 
 
-if __name__ == "__main__":
+async def create_task():
     asyncio.create_task(await dispatcher.start_polling())
+
+
+if __name__ == "__main__":
+    create_task()
